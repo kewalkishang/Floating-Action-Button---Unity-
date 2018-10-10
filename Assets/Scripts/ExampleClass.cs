d6012b58d5c7e19ce2493effb7fb8922d6b0590f
kewal kishan g <kewalkishang@gmail.com>
2018-10-11 01:39:12 +0530

    Basic Animations

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animation))]
+public class ExampleClass : MonoBehaviour
+{
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        AnimationCurve curve = AnimationCurve.Linear(0.0f, 0.0f,0.4f,3.0f);
+        AnimationClip clip = new AnimationClip();
+        clip.legacy = true;
+        clip.SetCurve("", typeof(Transform), "localScale.x", curve);
+        clip.SetCurve("", typeof(Transform), "localScale.y", curve);
+        anim.AddClip(clip, "test");
+        anim.Play("test");
+    }
+}
---
+++ Assets/Scripts/Transparency.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animation))]
+public class Transparency : MonoBehaviour {
+
+    public Animation anim;
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        AnimationCurve curve = AnimationCurve.Linear(0.0f, 0.0f, 2.0f, 1.0f);
+        AnimationClip clip = new AnimationClip();
+        clip.legacy = true;
+        clip.SetCurve("", typeof(GameObject), "renderer.material.color.a", curve);
+        anim.AddClip(clip, "test");
+        anim.Play("test");
+
+        Color col = GetComponent<Image>().color;
+        col.a = 1;
+        GetComponent<Image>().color = col;
+
+
+    }
+    void Update()
+    {
+
+    }
+    }
